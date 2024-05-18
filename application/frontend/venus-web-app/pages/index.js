962ca509947841832a3817a9126f13296f372b2c
Chamath Viduranga <chamath.viduranga@airudi.com>
2024-05-19 00:26:55 +0530

    add onepirate theme

--- application/frontend/venus-web-app/pages/index.js
+++ application/frontend/venus-web-app/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
+      <div>
+            <Button variant="contained">Click Me</Button>
+        </div>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
